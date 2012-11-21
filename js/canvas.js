6045698d68a2e87e84964741db1b944ca7c4451c
Thiago Rocha <kimobr@gmail.com>
2012-11-21 19:47:43 -0200

    Add css attrs to canvas.
    Add resize function to canvas.js.
    Centralize canvas area according to the user's window.

--- js/canvas.js
+++ js/canvas.js
@@ -10,12 +10,20 @@ var ImageCanvas = {
 		this.canvas = this.canvas = document.getElementById( 'canvas' );
     	this.context = canvas.getContext( '2d' );
     	
-    	this.canvas.height = this.height;
-    	this.canvas.width = this.width;
-    	this.canvas.style.display = 'block';
+    	if (window.innerHeight + 80 > window.innerWidth - 100)
+      	this.canvas.width = window.innerWidth - 100;
+    	else {
+      		this.canvas.width = window.innerHeight + 80;
+    	}
+    	this.canvas.height = window.innerHeight - 100;
+    	this.canvas.style.marginLeft = -this.canvas.width/2 + 'px';
+    	this.canvas.style.marginTop = -this.canvas.height/2 + 'px';
+    	this.canvas.style.display = 'block'
 	},
 
 	draw : function() {
+
+		this.resizeCanvas(width,height);
 
 		for(var i = 0; i < this.width; i++) {
 			for(var j = 0; j < this.height; j++) {
@@ -46,5 +54,34 @@ var ImageCanvas = {
     	this.context.fillRect(x,y,1,1);
     	this.context.closePath();
     	this.context.fill();
+	},
+
+	/*
+  	*  Resizes the canvas to fit within the screen, based on a given image width/height
+  	*/
+  	resizeCanvas: function( width, height ) {
+
+	    var newWidth, newHeight;
+
+	    var availableWidth = window.innerWidth - 100;
+	    var availableHeight = window.innerHeight - 100;
+
+	    // If the image is too big for the screen... scale it down.
+	    if ( width > availableWidth || height > availableHeight ) {
+
+	      var maxRatio = Math.max( width / availableWidth , height / availableHeight );
+	      newWidth = width / maxRatio;
+	      newHeight = height / maxRatio;
+
+	    } 
+	    else {
+	      newWidth = width;
+	      newHeight = height;
+	    }
+
+	    this.canvas.width = newWidth//window.innerWidth;
+	    this.canvas.height = newHeight//window.innerHeight;
+	    this.canvas.style.marginLeft = -this.canvas.width/2 + 'px';
+	    this.canvas.style.marginTop = -this.canvas.height/2 + 'px';
 	}
 }

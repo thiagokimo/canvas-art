626d73614621633e484b2e9929712cd66653f15b
Kimo <kimobr@gmail.com>
2012-11-21 09:51:57 -0200

    Changed the whole logic.
    The code was adapted to use Sierpinsky Carpet.

--- paint.js
+++ paint.js
@@ -5,71 +5,34 @@ var ctx = c.getContext('2d');
 c.width = width;
 c.height = height;
 
-function init(){	
-	draw(0,0,width,height);
-}
-function draw(x,y,w,h,start){
-
-	var newWidth;
-	var newHeight; 
-
-	//condition to the first time
-	if(start){
-
-		if (w == width && h == height) {
-			
-			drawTicTacToe(y,x,w,h);
-
-			newWidth = calculateDelta(w);
-			newHeight = calculateDelta(h);
-
-			draw(x,y,newWidth,newHeight,false);
+function init(){
+	
+	for(var i = 0; i < width; i++) {
+		for(var j = 0; j < height; j++) {
+			if(isSierpinskiCarpetPixelFilled(i,j)) {
+				drawRect(i,j,'#000000');
+			}
 		}
-		
 	}
-	else {
-
-		//stop condition
-		if(w > 0 && h > 0){
-			
-			drawTicTacToe(x,y,w,h);
-
-			newWidth = calculateDelta(w);
-			newHeight = calculateDelta(h);
-
-			draw(x,y,newWidth,newHeight,false);
-		}
-			
-	}
-
 }
 
-function drawTicTacToe(x, y, w, h){
-
- 	var widthDelta = calculateDelta(w);
- 	var heightDelta = calculateDelta(h);
-  
- 	//vertical lines
- 	ctx.moveTo(x + widthDelta, y);
- 	ctx.lineTo(x + widthDelta, y+h);
- 	ctx.stroke();
-
- 	ctx.moveTo(x + (2 * widthDelta), y);
- 	ctx.lineTo(x + (2 * widthDelta), y+h);
- 	ctx.stroke();
-
- 	//horizontal
- 	ctx.moveTo(x, y + heightDelta);
- 	ctx.lineTo(x + w, y + heightDelta);
- 	ctx.stroke();
-
- 	ctx.moveTo(x, y + (2 * heightDelta));
- 	ctx.lineTo(x + w, y + (2 * heightDelta));
- 	ctx.stroke();
+function isSierpinskiCarpetPixelFilled(x,y) {
+	while(x>0&&y>0) //when either of these reaches zero the pixel is determined to be on the edge at that square level and must be filled
+        {
+            if(x%3===1 && y%3===1) //checks if the pixel is in the center for the current square level
+                return 0;
+            x = Math.floor(x/3); //x and y are decremented to check the next larger square level
+            y = Math.floor(y/3);
+        }
+        return 1; //if all possible square levels are checked and the pixel is not determined to be open it must be filled
 }
 
-function calculateDelta(value) {
-	return parseInt(value/3);
+function drawRect (x,y,color) {
+    ctx.fillStyle=color;
+    ctx.beginPath();
+    ctx.fillRect(x,y,1,1);
+    ctx.closePath();
+    ctx.fill();
 }
 
 init();

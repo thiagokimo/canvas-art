52991346c71e7dad378d14a3b2afc3fca38141b8
Thiago Rocha <kimo@kimo.io>
2013-07-18 10:39:57 -0300

    Hidding dat.gui setup

---
+++ js/datgui.js
@@ -0,0 +1,17 @@
+var datGUI = {
+  setup : function () {
+    var gui = new dat.GUI();
+    var f1 = gui.addFolder('Parameters:');
+
+    f1.addColor(ImageCanvas, 'color').name('Color Picker');
+    f1.add(ImageCanvas, 'fillFactor').min(2).name('Fill Factor');
+    f1.add(ImageCanvas, 'XDecrement').min(2).name('X Axis Value');
+    f1.add(ImageCanvas, 'YDecrement').min(2).name('Y Axis Value');
+
+    gui.add(ImageCanvas, "clear").name('Clear Canvas');
+    gui.add(ImageCanvas, "save").name('Save');
+    gui.add(ImageCanvas, "draw").name('Draw');
+
+    f1.open();
+  }
+}

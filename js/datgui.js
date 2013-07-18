27dec4e97903cf7dc1ba9aa1295baace4b0fdfe2
Thiago Rocha <kimo@kimo.io>
2013-07-18 12:34:00 -0300

    Drawing on any parameters change

--- js/datgui.js
+++ js/datgui.js
@@ -4,14 +4,30 @@ var datGUI = {
     var f1 = gui.addFolder('Parameters:');
 
     f1.addColor(ImageCanvas, 'color').name('Color Picker');
-    f1.add(ImageCanvas, 'fillFactor').min(2).name('Fill Factor');
-    f1.add(ImageCanvas, 'XDecrement').min(2).name('X Axis Value');
-    f1.add(ImageCanvas, 'YDecrement').min(2).name('Y Axis Value');
+    f1.add(ImageCanvas, 'fillFactor').min(2).name('Fill Factor').onChange(function (value) {
+
+        ImageCanvas.fillFactor = value;
+        ImageCanvas.draw();
+
+    });
+    f1.add(ImageCanvas, 'XDecrement').min(2).name('X Axis Value').onChange(function (value) {
+
+        ImageCanvas.XDecrement = value;
+        ImageCanvas.draw();
+
+    });
+    f1.add(ImageCanvas, 'YDecrement').min(2).name('Y Axis Value').onChange(function (value) {
+
+        ImageCanvas.YDecrement = value;
+        ImageCanvas.draw();
+
+    });
 
     gui.add(ImageCanvas, "clear").name('Clear Canvas');
     gui.add(ImageCanvas, "save").name('Save');
-    gui.add(ImageCanvas, "draw").name('Draw');
 
     f1.open();
+
+    ImageCanvas.draw();
   }
 }
